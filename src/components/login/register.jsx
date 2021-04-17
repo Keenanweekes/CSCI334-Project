ba33f878fb3400b2958e3c7207ceb0ba746b82db
Albert <albertiannelli@gmail.com>
2021-04-18 07:50:28 +1000

    Sign up page update
    All sign up fields are now present in the form and save to firebase

--- src/components/login/register.jsx
+++ src/components/login/register.jsx
@@ -17,6 +17,7 @@ var firebaseConfig = {
   appId: "1:971326205509:web:d9fafc407b675d118fbd2c",
   measurementId: "G-FY11MJT3LH"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
@@ -25,26 +26,48 @@ var firestore = firebase.firestore();
 
 const docRef = firestore.collection("users").doc("test");
 
-
-
 export class Register extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+
+    //blank react state values which will be updates depending on what the user types into the text field
+    this.state = ({
+      username: "",
+      password: "",
+      firstname: "",
+      lastname: "",
+      mobile: "",
+      email: "",
+      dob: "",
+      address: "",
+      type: ""
+    })
 
     this.handleChange = this.handleChange.bind(this);
     this.createRecord = this.createRecord.bind(this);
   }
 
-  handleChange(event)
+  //handle change function which takes whatever the user inout is in any of the fields, 
+  //matches the same react state with the name of the field and updates that react state with the users input
+  handleChange = (event) =>
   {
-    this.setState({value: event.target.value});
-  }
+    const value = event.target.value
+    this.setState({...this.state,[event.target.name]: value});}
 
+  //when submit is pressed it gathers all of the user input fields that were saved to the react state and saves them into firebase
   createRecord(event)
   {
     docRef.set({
-      username: this.state.value}
+      username: this.state.username,
+      password: this.state.password,
+      firstname: this.state.firstname,
+      lastname: this.state.lastname,
+      mobile: this.state.mobile,
+      email: this.state.email,
+      dob: this.state.dob,
+      address: this.state.address,
+      type: this.state.type
+    }
       ).then(function (){
         console.log("Success");
       }).catch(function(error){
@@ -54,6 +77,7 @@ export class Register extends React.Component {
   }
 
   render() {
+    
     return (
       <div className="base-container" ref={this.props.containerRef}>
         <script src="https://www.gstatic.com/firebasejs/8.3.3/firebase-app.js"></script>
@@ -67,7 +91,28 @@ export class Register extends React.Component {
           <div className="form">
             <form className="form-group" onSubmit={this.createRecord}>
               <label htmlFor="username">Username</label>
-              <input type="text" name="username" placeholder="username" value = {this.state.value} onChange = {this.handleChange}/>
+              <input type="text" name="username" placeholder="username" value = {this.state.username} onChange = {this.handleChange}/>
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" placeholder="password" value = {this.state.password} onChange = {this.handleChange}/>
+              <label htmlFor="firstname">FirstName</label>
+              <input type="text" name="firstname" placeholder="firstname" value = {this.state.firstname} onChange = {this.handleChange}/>
+              <label htmlFor="lastname">LastName</label>
+              <input type="text" name="lastname" placeholder="lastname" value = {this.state.lastname} onChange = {this.handleChange}/>
+              <label htmlFor="mobile">Mobile</label>
+              <input type="text" name="mobile" placeholder="mobile" value = {this.state.mobile} onChange = {this.handleChange}/>
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" placeholder="email" value = {this.state.email} onChange = {this.handleChange}/>
+              <label htmlFor="dob">Date Of Birth</label>
+              <input type="text" name="dob" placeholder="Date Of Birth" value = {this.state.dob} onChange = {this.handleChange}/>
+              <label htmlFor="address">Address</label>
+              <input type="text" name="address" placeholder="Address" value = {this.state.address} onChange = {this.handleChange}/>
+              <label htmlFor="type">User Type</label>
+
+              <select name = "type" value = {this.state.type} onChange = {this.handleChange}>
+                <option value = "contacttracer">Contact Tracer</option>
+                <option value = "healthcare">HealthCare Worker</option>
+                <option value = "general public">General Public Member</option>
+              </select>
               <input type = "submit" value = "Submit"/>
             </form>
 
