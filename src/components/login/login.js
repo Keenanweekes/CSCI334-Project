01801e55f23c1b9593b57aa455f7a20f60ea3f30
JasonMartin588 <65000427+JasonMartin588@users.noreply.github.com>
2021-04-23 14:26:13 +1000

    Login css completed

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,6 +1,7 @@
 import React, { Component, useState } from "react";
 import Register from "./register";
 import Test from "./test";
+import './login.css';
 
 const Login = () => {
   const [state, setstate] = useState('start');
@@ -8,46 +9,50 @@ const Login = () => {
   return (
     <div className="base-container" >
       {state === 'start' && (
-      <div className="login-stuff">
-        <h1 className="login-header">COVID RESPONSE</h1>
-        <h2 className="login-box-header">LOG IN</h2>
-        <div className="content">
-          <div className="form">
-            <div className="form-group">
-              <input type="text" name="email" placeholder="Email" />
+        <div className="login-main">
+          <h1 className="login-header">COVID RESPONSE</h1>
+          <div className="login-box">
+          <h2 className="login-box-header">LOG IN</h2>
+            <div className="form">
+              <div className="form-group">
+                <input type="text" name="email" placeholder="Email" />
+              </div>
+              <div className="form-group">
+                <input type="password" name="password" placeholder="Password" />
+              </div>
+              <div className="show-password">
+                <input type="checkbox" name="show-password-checkbox" value="Show Password"></input>
+                <label for="show-password-checkbox">Show Password</label>
+              </div>
+              <div className="remember-me">
+                <input type="checkbox" name="remember-me-checkbox" value="Remember Me"></input>
+                <label for="remember-me-checkbox">Remember Me</label>
+              </div>
+          </div>
+          <div className="buttons">
+            <div className="sign-in-button-container">
+            <button type="button" className="sign-in-button" onClick={() => setstate("Test")}>
+              Sign In
+            </button>
             </div>
-            <div className="form-group">
-              <input type="password" name="password" placeholder="Password" />
-            </div>
-            <div className="show-password">
-              <input type="checkbox" name="show-password-checkbox" value="Show Password"></input>
-              <label for="show-password-checkbox">Show Password</label>
-            </div>
-            <div className="remember-me">
-              <input type="checkbox" name="remember-me-checkbox" value="Remember Me"></input>
-              <label for="remember-me-checkbox">Remember Me</label>
+            <div className="sign-in-button-container">
+            <button type="button" className="sign-up-button" onClick={() => setstate("Register")}>Sign Up</button>
             </div>
           </div>
-        </div>
-        <div className="buttons">
-          <button type="button" className="sign-in-button" onClick={() => setstate("Test")}>
-            Sign In
-          </button>
-          <button type="button" className="sign-up-button" onClick={() => setstate("Register")}>Sign Up</button>
-        </div>
-        <div className="password-recovery">
-          <a className="password-recovery-text">Forgotten Password?</a>
-        </div>
-        <footer className="log-in-footer">
-          <a className="privacy-policy">Privacy Policy</a>
-          <a className="terms-of-use">Terms of Use</a>
-        </footer>
+          <div className="password-recovery">
+            <a className="password-recovery-text">Forgotten Password?</a>
+          </div>
+          </div>
+          <footer className="login-footer">
+            <a className="privacy-policy">Privacy Policy</a>
+            <a className="terms-of-use">Terms of Use</a>
+          </footer>
         </div>)}
 
-        <div>
-          {state === "Test" && <Test />}
-          {state === "Register" && <Register />}
-        </div>
+      <div className="state">
+        {state === "Test" && <Test />}
+        {state === "Register" && <Register />}
+      </div>
 
     </div>
 
--- src/components/login/register.js
+++ src/components/login/register.js
@@ -1,13 +1,60 @@
 import React, {Component, useState} from "react";
 import loginImg from "../../login.svg";
 import Test from "./test";
+import Login from "./login";
+
 
 const Register = () =>{
-  const [state, setstate] = useState('start');
+  const [state, setstate] = useState('register');
   
   return (
-    
-    <h1>Register</h1>
+    <div className="register-base-container">
+        {state === 'register' && (
+          <div className="register-main">
+            <h1 className="register-header">COVID RESPONSE</h1>
+            <h2 className="register-box-header">SIGN UP</h2>
+            <div className="form">
+              <div className="form-group">
+                <input type="text" name="email" placeholder="Email"></input>
+              </div>
+              <div className="form-group">
+                <input type="text" name="username" placeholder="Username"></input>
+              </div>
+              <div className="form-group">
+                <input type="password" name="password" placeholder="Password"></input>
+              </div>
+              <div className="form-group">
+                <input type="text" name="first-name" placeholder="First Name"></input>
+              </div>
+              <div className="form-group">
+                <input type="text" name="last-name" placeholder="Last Name"></input>
+              </div>
+              <div className="form-group">
+                <input type="text" name="mobile" placeholder="Mobile Number"></input>
+              </div>
+              <div className="form-group">
+                <input type="text" name="dob" placeholder="Date of Birth"></input>
+              </div>
+              <div className="form-group">
+                <input type="text" name="address" placeholder="Address"></input>
+              </div>
+            </div>
+            <div className="buttons">
+              <button className="sign-up-button" onClick={() => setstate("Login")}>Sign Up</button>
+              <button className="back-button" onClick={() => setstate("Login")}>Back</button>
+            </div>
+          <footer className="register-footer">
+            <a className="privacy-policy">Privacy Policy</a>
+            <a className="terms-of-use">Terms of Use</a>
+          </footer>
+
+        </div>)}
+
+        <div>
+          {state === "Login" && <Login />}
+        </div>
+
+    </div>
 
   );
   }
